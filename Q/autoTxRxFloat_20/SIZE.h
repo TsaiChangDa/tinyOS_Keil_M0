d1781078e71ce91d34b6961b44ed4811748cd206
TsaiChangDa <109889120+TsaiChangDa@users.noreply.github.com>
2022-07-25 21:56:50 +0800

    Add files via upload

---
+++ Q/autoTxRxFloat_20/SIZE.h
@@ -0,0 +1,39 @@
+
+#include "PORT.h"
+
+	  // task
+#define  TASKSIZE           2
+#define  STACKRAMBYTES   2000
+
+    // event and service
+#define  PENDSIZE           0
+#define  MEMORYPOOLBYTES  100
+#define  GETMEMORYSIZE      0
+#define  QSIZE              1   
+#define  QBYTES             1	
+#define  MAILSIZE           0
+#define  FLAGSIZE           0	
+#define  MUTEXSIZE          0
+#define  UNTILSIZE          0
+
+    // padding part of task stack(selective)
+int PaddingOS[TASKSIZE+1] = {0}; 
+#define  PADDINGIDLE       0      // idleTaskOS() padding
+
+#if   defined ( ARM )     
+  #define  WORDSIZE    4
+       // core register address
+  #define SystickControlRegisterOS       ( *( ( volatile unsigned int* ) 0xE000E010 ) )
+  #define SystickLoadRegisterOS          ( *( ( volatile unsigned int* ) 0xE000E014 ) )
+  #define SystickCurrentValueRegisterOS  ( *( ( volatile unsigned int* ) 0xE000E018 ) )
+  #define InterruptControlRegisterOS     ( *( ( volatile unsigned int* ) 0xE000ED04 ) )
+  #define HandlerPriorityRegisterOS      ( *( ( volatile unsigned int* ) 0xE000ED20 ) )  //SHPR3
+#endif
+
+#if   defined ( CM0 )
+	#define  CPUREGISTER         17
+#endif
+	
+
+
+
---
+++ Q/variousData_10/main.c
@@ -0,0 +1,192 @@
+
+#include "OS.h"
+
+#define  RDR    (1<<0)
+#define  THRE   (1<<5)
+#define  TEMT   (1<<6)
+
+
+void  initializeUART(int baudrate)
+{
+   int Fdiv;
+   int regVal;
+
+   NVIC_DisableIRQ(UART_IRQn);
+   LPC_SYSCON->SYSAHBCLKCTRL |= (1<<16);	// IOCON clock enable 	
+   LPC_IOCON->PIO1_6 |= 0x01;    // RX   PIN-15
+   LPC_IOCON->PIO1_7 |= 0x01;    // TX   PIN-16
+   LPC_SYSCON->SYSAHBCLKCTRL |= (1<<12);	// UART clock enable   
+   LPC_SYSCON->UARTCLKDIV = 0x1;    
+   LPC_UART->LCR = 0x83;           
+   regVal = LPC_SYSCON->UARTCLKDIV;								
+   Fdiv = (((SystemCoreClock*LPC_SYSCON->SYSAHBCLKDIV)/regVal)/16)/baudrate ;
+   LPC_UART->DLM = Fdiv / 256;							
+   LPC_UART->DLL = Fdiv % 256;
+   LPC_UART->LCR = 0x03;		
+   LPC_UART->FCR = 0x07;		
+   regVal = LPC_UART->LSR; 
+   while (( LPC_UART->LSR & (THRE|TEMT)) != (THRE|TEMT) );
+   while ( LPC_UART->LSR & RDR )
+   {
+	    regVal = LPC_UART->RBR;	
+   }
+   NVIC_EnableIRQ(UART_IRQn);
+   LPC_UART->IER= (1<<0);     
+} //  initUART(int baudrate)
+
+
+void sendByte(char data)
+{
+  	while ( !(LPC_UART->LSR & THRE) ); // wait until Tx FIFO is empty
+  	LPC_UART->THR = data;
+	
+  	while ( !(LPC_UART->LSR & THRE) ); // wait until Tx FIFO is empty
+  	LPC_UART->THR = ' ';	
+}
+
+
+
+void transfer(unsigned char  p )
+{
+	  unsigned char  x;
+	
+	  if (p < 10) // 0,1 .....9
+	  {
+		  x = p + 48 ;
+	  }
+	  else  // 10, 11,....,15
+	  {
+        x = p + 87;  // a,b,¡K¡K,f
+	  }	
+    sendByte(x);
+}
+
+void print32bits(unsigned  int  y)
+{
+    unsigned char  p;
+
+	  sendByte( '0' );
+	  sendByte( 'x' );	
+	  p = (y & 0xF0000000) >>28;
+    transfer(p);
+	  p = (y & 0x0F000000) >>24;
+    transfer(p);
+	  p = (y & 0x00F00000) >>20;
+    transfer(p);
+	  p = (y & 0x000F0000) >>16;
+    transfer(p);
+    p = (y & 0x0000F000) >>12;
+    transfer(p);
+	  p = (y & 0x00000F00) >>8;
+    transfer(p);
+	  p = (y & 0x000000F0) >>4;
+    transfer(p);
+	  p = (y & 0x0000000F) >>0;
+    transfer(p);
+		sendByte(' ');
+}
+
+int     *pint;
+char    *pchar;
+float   *pfloat;
+double  *pdouble;
+void**  pvoid;
+
+int     x = 0;
+float   y = 1.024;
+
+void task0(void)  
+{	
+   while(1)
+   {
+		   sendByte('A');
+       pint = getMemoryOS(0, 4);
+		   *pint = x;
+		 	 postQOS(0, pint);
+
+		   pchar = getMemoryOS(1, 1);	
+		   *pchar = 'a' + x;
+		 	 postQOS(0, pchar);
+		   x++;
+ 
+		   pdouble = getMemoryOS(2, 8);				 
+		   *pdouble     = 0.00000016;
+		   *(pdouble+1) = 0.00000032;
+		 	 postQOS(0, pdouble);		 
+ 
+		   delayTickOS(15);
+   }
+}
+
+void task1(void)  
+{
+   while(1)
+   {
+		   sendByte('B');
+       pfloat = getMemoryOS(3, 4);	 
+		   *pfloat = y;
+		   y += 0.001;
+		 	 postQOS(1,pfloat); 
+		   delayTickOS(15);
+   }
+}
+
+void task2(void)  
+{	 
+	 int     number[]={1, 0, -1};
+	 double  w1, w2;
+	 int     items;
+	 
+   while(1)
+   {
+		   sendByte('C');
+		 	 pvoid = pendQOS(number, 1, &items, INFINITEOS );
+		   if ( qReadyNumberOS(pvoid) == 1 )
+			 {
+           pfloat = *pvoid;	
+				 	 print32bits(  (int)(*pfloat * 1000) );	
+		 	 }
+
+       pvoid = readQOS(0, &items);
+		   pint    =  (int*)    *pvoid;
+		   pchar   =  (char*)   *(pvoid + 1);
+		   pdouble =  (double*) *(pvoid + 2);
+				 
+			 sendByte('#');
+		 	 sendByte( '0' + *pint );	
+		 	 sendByte( *pchar );	
+		 
+		 	 w1 = *pdouble * 100000000.0;              // 16 , 0x10 
+			 w2 = *(pdouble + 1) * 100000000.0;			   // 32 , 0x20
+		 	 print32bits( (int)w1  );	
+		 	 print32bits( (int)w2  );				 
+
+			 putMemoryOS(0);
+			 putMemoryOS(1);
+			 putMemoryOS(2);
+			 putMemoryOS(3);	
+
+    }
+}
+
+void (*taskName[])(void)={task0, task1, task2};
+
+      // ErrorCode : 1- TaskCountOS != TASKSIZE+1
+int main(void)  
+{
+	   char errorCode;
+	   int startTaskIndex;
+	   int arraySize;
+	
+	   initializeUART(9600);   // 600 bytes
+	
+	   arraySize = sizeof(taskName) / sizeof(taskName[0]);
+     startTaskIndex = 0;
+     errorCode = startOS(taskName, arraySize, startTaskIndex, CLOCKOS, NULL); 
+		 sendByte('0'+ errorCode);	// never execute if start successfully
+	 
+} // main
+
+
+
+
