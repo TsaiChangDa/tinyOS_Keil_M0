51124426d9f0469a3e3bccd8852a54296a63cd51
TsaiChangDa <109889120+TsaiChangDa@users.noreply.github.com>
2022-07-29 16:39:05 +0800

    Add files via upload

---
+++ otherPort/STM32F0/PORT.h
@@ -0,0 +1,10 @@
+#ifndef _PORT_H
+#define _PORT_H
+
+#define   KEIL   // compiler : GNU, IAR, KEIL
+#define   ARM    // CPU architecture
+#define   CM0	   // processor
+#define   NXP    // manufactory : NXP, STM
+
+#endif
+
---
+++ otherPort/STM32F0/sem3/STM32F0DisCubeMx2/main.c
@@ -0,0 +1,403 @@
+
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  ** This notice applies to any and all portions of this file
+  * that are not between comment pairs USER CODE BEGIN and
+  * USER CODE END. Other portions of this file, whether 
+  * inserted by the user or by software development tools
+  * are owned by their respective copyright owners.
+  *
+  * COPYRIGHT(c) 2022 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stm32f0xx_hal.h"
+#include "os.h"
+
+#include "os.c"
+#include "inline.c"
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart1;
+
+/* USER CODE BEGIN PV */
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART1_UART_Init(void);
+
+/* USER CODE BEGIN PFP */
+/* Private function prototypes -----------------------------------------------*/
+
+/* USER CODE END PFP */
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  *
+  * @retval None
+  */
+	
+void sendByte(uint8_t c)
+{
+	  uint8_t str[1];
+	  uint8_t space[1] ={' '};
+		
+	  str[0] = c;
+    HAL_UART_Transmit(&huart1, str, 1, 10000);
+    HAL_UART_Transmit(&huart1, space, 1, 10000);	
+}
+
+void sendStr(uint8_t *str, uint16_t size)
+{
+    HAL_UART_Transmit(&huart1, str, size, 10000);
+	  sendByte(' ');
+}
+
+void transfer(unsigned char  p )
+{
+	  unsigned char  x;
+	
+	  if (p < 10) // 0,1 .....9
+	  {
+		  x = p + 48 ;
+	  }
+	  else  // 10, 11,....,15
+	  {
+        x = p + 87;  // a,b,¡K¡K,f
+	  }	
+    sendByte(x);
+}
+
+void print32bits(unsigned  int  y)
+{
+    unsigned char  p;
+
+	  sendByte( '0' );
+	  sendByte( 'x' );	
+	  p = (y & 0xF0000000) >>28;
+    transfer(p);
+	  p = (y & 0x0F000000) >>24;
+    transfer(p);
+	  p = (y & 0x00F00000) >>20;
+    transfer(p);
+	  p = (y & 0x000F0000) >>16;
+    transfer(p);
+    p = (y & 0x0000F000) >>12;
+    transfer(p);
+	  p = (y & 0x00000F00) >>8;
+    transfer(p);
+	  p = (y & 0x000000F0) >>4;
+    transfer(p);
+	  p = (y & 0x0000000F) >>0;
+    transfer(p);
+		sendByte(' ');
+}
+
+
+extern unsigned int* TaskSpPointerOS[5];
+ 
+
+void task0(void)  
+{
+   int n[]={0, -1};
+
+	print32bits( (unsigned int) autoPackItemsOS() ); 
+	 
+   while(1) 
+   {  
+		  sendByte('A');
+      pendSemOS(n, TIMEOUT_INFINITE  );
+     print32bits(  (unsigned int) __get_PSP()); 
+ 		  print32bits( (unsigned int) *TaskSpPointerOS[0]);	
+		  print32bits( (unsigned int) TaskSpPointerOS[0]); 
+		  postSemOS(1); 
+   }  
+} 
+
+void task1(void)  
+{ 
+   int n[]={1, -1};
+	 
+   while(1)
+   { 
+		  sendByte('B');
+      pendSemOS(n, TIMEOUT_INFINITE  ); 
+      print32bits(  (unsigned int) __get_PSP()); 
+ 		  print32bits( (unsigned int) *TaskSpPointerOS[1]);	
+		  print32bits( (unsigned int) TaskSpPointerOS[1]); 
+      postSemOS(2); 
+   }  
+} 
+
+void task2(void)  
+{
+   int n[]={2, -1};
+
+   while(1)
+   { 
+		  sendByte('C');
+      pendSemOS(n, TIMEOUT_INFINITE  );
+      print32bits(  (unsigned int) __get_PSP()); 
+ 		  print32bits( (unsigned int) *TaskSpPointerOS[2]);	
+		  print32bits( (unsigned int) TaskSpPointerOS[2]); 
+      postSemOS(3); 
+   }  
+} 
+
+void task3(void)  
+{	
+	  print32bits( (unsigned int) TaskSpPointerOS[4]);	
+	
+	 while(1)
+   { 
+     sendByte('D');	
+     print32bits(  (unsigned int) __get_PSP()); 
+ 		 print32bits( (unsigned int) *TaskSpPointerOS[3]);	
+		 print32bits( (unsigned int) TaskSpPointerOS[3]); 
+		  sendByte('D');	
+      postSemOS(0); 
+		  delayTimeOS(0,0,1,0);
+   } 
+} 
+
+
+void (*taskName[])(void)={task0,task1,task2,task3};
+
+
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+     char errorCode;
+	   int startTaskIndex;
+	   int arraySize;
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration----------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+      HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+      SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+      MX_GPIO_Init();
+      MX_USART1_UART_Init();
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+      HAL_GPIO_TogglePin(GPIOC, LD3_Pin);
+      arraySize = sizeof(taskName) / sizeof(taskName[0]);
+      startTaskIndex = 0;
+      errorCode = startOS(taskName, arraySize, startTaskIndex, clockOS);  // create idleTaskOS()
+		  sendByte('0'+errorCode);
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_PeriphCLKInitTypeDef PeriphClkInit;
+
+    /**Initializes the CPU, AHB and APB busses clocks 
+    */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = 16;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL12;
+  RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV1;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+    /**Initializes the CPU, AHB and APB busses clocks 
+    */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1;
+  PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK1;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+    /**Configure the Systick interrupt time 
+    */
+  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+
+    /**Configure the Systick 
+    */
+  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+  /* SysTick_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+}
+
+/* USART1 init function */
+static void MX_USART1_UART_Init(void)
+{
+
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 9600;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+}
+
+/** Configure pins as 
+        * Analog 
+        * Input 
+        * Output
+        * EVENT_OUT
+        * EXTI
+*/
+static void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, LD4_Pin|LD3_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : B1_Pin */
+  GPIO_InitStruct.Pin = B1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_EVT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : LD4_Pin LD3_Pin */
+  GPIO_InitStruct.Pin = LD4_Pin|LD3_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @param  file: The file name as string.
+  * @param  line: The line in file as a number.
+  * @retval None
+  */
+void _Error_Handler(char *file, int line)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  while(1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
