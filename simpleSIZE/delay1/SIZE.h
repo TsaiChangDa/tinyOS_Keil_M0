30b367b19bcaad8cf7d635bbdb85b2311c6ba4d9
TsaiChangDa <109889120+TsaiChangDa@users.noreply.github.com>
2022-12-10 19:20:54 +0800

    Add files via upload

--- simpleSIZE/delay1/SIZE.h
+++ simpleSIZE/delay1/SIZE.h
@@ -1,21 +1,6 @@
-// configure TASKSIZE only
 
-#include "PORT.h"
+// rename simpleSIZE.h as SIZE.h
 
-#if   defined ( ARM )     
-  #define  WORDSIZE    4
-       // core register address
-  #define SystickControlRegisterOS       ( *( ( volatile unsigned int* ) 0xE000E010 ) )
-  #define SystickLoadRegisterOS          ( *( ( volatile unsigned int* ) 0xE000E014 ) )
-  #define SystickCurrentValueRegisterOS  ( *( ( volatile unsigned int* ) 0xE000E018 ) )
-  #define InterruptControlRegisterOS     ( *( ( volatile unsigned int* ) 0xE000ED04 ) )
-  #define HandlerPriorityRegisterOS      ( *( ( volatile unsigned int* ) 0xE000ED20 ) )  //SHPR3
-#endif
-
-#if   defined ( CM0 )
-	#define  CPUREGISTER         17
-#endif
-	
 	  // task
 #define  TASKSIZE          2 
 #define  STACKRAMBYTES     (CPUREGISTER + 40)*WORDSIZE*(TASKSIZE + 1)
