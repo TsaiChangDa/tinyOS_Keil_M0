1489c38156a4e27e7b9d59de643f13b099a9da9b
TsaiChangDa <109889120+TsaiChangDa@users.noreply.github.com>
2022-12-10 19:32:27 +0800

    Add files via upload

--- startError/taskSize_1/SIZE.h
+++ startError/taskSize_1/SIZE.h
@@ -1,5 +1,4 @@
 
-#include "PORT.h"
 
 	  // task
 #define  TASKSIZE           3 
@@ -19,21 +18,6 @@
     // padding part of task stack(selective)
 int PaddingOS[TASKSIZE+1] = {0}; 
 #define  PADDINGIDLE       0      // idleTaskOS() padding
-
-#if   defined ( ARM )     
-  #define  WORDSIZE    4
-       // core register address
-  #define SystickControlRegisterOS       ( *( ( volatile unsigned int* ) 0xE000E010 ) )
-  #define SystickLoadRegisterOS          ( *( ( volatile unsigned int* ) 0xE000E014 ) )
-  #define SystickCurrentValueRegisterOS  ( *( ( volatile unsigned int* ) 0xE000E018 ) )
-  #define InterruptControlRegisterOS     ( *( ( volatile unsigned int* ) 0xE000ED04 ) )
-  #define HandlerPriorityRegisterOS      ( *( ( volatile unsigned int* ) 0xE000ED20 ) )  //SHPR3
-#endif
-
-#if   defined ( CM0 )
-	#define  CPUREGISTER         17
-#endif
-	
 
 
 
