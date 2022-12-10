4e221d77eada5d9232f05457e47ec04fb6d6d056
TsaiChangDa <109889120+TsaiChangDa@users.noreply.github.com>
2022-12-10 19:22:54 +0800

    Add files via upload

--- simpleSIZE/postFlag_2/SIZE.h
+++ simpleSIZE/postFlag_2/SIZE.h
@@ -1,20 +1,6 @@
 
-#include "PORT.h"
 
-#if   defined ( ARM )     
-  #define  WORDSIZE    4
-       // core register address
-  #define SystickControlRegisterOS       ( *( ( volatile unsigned int* ) 0xE000E010 ) )
-  #define SystickLoadRegisterOS          ( *( ( volatile unsigned int* ) 0xE000E014 ) )
-  #define SystickCurrentValueRegisterOS  ( *( ( volatile unsigned int* ) 0xE000E018 ) )
-  #define InterruptControlRegisterOS     ( *( ( volatile unsigned int* ) 0xE000ED04 ) )
-  #define HandlerPriorityRegisterOS      ( *( ( volatile unsigned int* ) 0xE000ED20 ) )  //SHPR3
-#endif
 
-#if   defined ( CM0 )
-	#define  CPUREGISTER         17
-#endif
-	
 	  // task
 #define  TASKSIZE          5 
 #define  STACKRAMBYTES     (CPUREGISTER + 40)*WORDSIZE*(TASKSIZE + 1)
