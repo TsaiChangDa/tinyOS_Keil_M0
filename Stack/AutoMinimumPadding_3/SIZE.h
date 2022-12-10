27d661a685d32c525ce56450db048c9deae24893
TsaiChangDa <109889120+TsaiChangDa@users.noreply.github.com>
2022-12-10 19:24:12 +0800

    Add files via upload

--- Stack/AutoMinimumPadding_3/SIZE.h
+++ Stack/AutoMinimumPadding_3/SIZE.h
@@ -1,11 +1,10 @@
 
-#include "PORT.h"
+
 
 	  // task
 #define  TASKSIZE           5 
 //#define  STACKRAMBYTES   2500 
-#define  STACKRAMBYTES   0x774
-//#define  STACKRAMBYTES   0x760
+#define  STACKRAMBYTES   0x760
     // event and service
 #define  PENDSIZE           0
 #define  MEMORYPOOLBYTES    0
@@ -20,20 +19,4 @@
     // padding part of task stack(selective)
 int PaddingOS[TASKSIZE+1] = {0}; 
 #define  PADDINGIDLE       0      // idleTaskOS() padding
-
-#if   defined ( ARM )     
-  #define  WORDSIZE    4
-       // core register address
-  #define SystickControlRegisterOS       ( *( ( volatile unsigned int* ) 0xE000E010 ) )
-  #define SystickLoadRegisterOS          ( *( ( volatile unsigned int* ) 0xE000E014 ) )
-  #define SystickCurrentValueRegisterOS  ( *( ( volatile unsigned int* ) 0xE000E018 ) )
-  #define InterruptControlRegisterOS     ( *( ( volatile unsigned int* ) 0xE000ED04 ) )
-  #define HandlerPriorityRegisterOS      ( *( ( volatile unsigned int* ) 0xE000ED20 ) ) //SHPR3
-#endif
-
-#if   defined ( CM0 )
-	#define  CPUREGISTER         17
-#endif
-	
-
 
